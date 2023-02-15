6942db78f6e9620e36bf5e5767525f7f597c3003
YouriSystem4 <youri.gruiters@system4.nl>
2023-02-15 18:54:20 +0100

    feat(tailwindcss): add tailwindCSS usage

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Root = () => {
   return (
     <div>
-      <div className="root">Root</div>
+      <div className="bg-red-500 w-full h-10">Root</div>
       <div className="outlet">
         <Outlet />
       </div>
