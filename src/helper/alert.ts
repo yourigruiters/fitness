eb1ae58d5accbc5f69ddc675372e2f4f3e053ad2
YouriSystem4 <youri.gruiters@system4.nl>
2023-03-01 20:41:39 +0100

    feat(alert): added all translations to Alerts

--- src/helper/alert.ts
+++ src/helper/alert.ts
@@ -1,35 +1,56 @@
+import { useIntl } from "react-intl";
+
 export const getAlertContent = (issue: number | string) => {
   console.log("getAlertContent", issue);
+
+  const intl = useIntl();
+
   switch (issue) {
     case "auth/password-reset":
       return {
-        title: "Password reset send",
-        description:
-          "A password reset link has been succesfully send to your email address.",
+        title: intl.formatMessage({
+          id: "alert.titlePassReset",
+        }),
+        description: intl.formatMessage({
+          id: "alert.descriptionPasswordReset",
+        }),
         type: "success",
       };
     case "auth/email-already-in-use":
       return {
-        title: "Email already in use",
-        description:
-          "This email address is already in use, please login to your account or use a different email address.",
+        title: intl.formatMessage({
+          id: "alert.titleEmailInUse",
+        }),
+        description: intl.formatMessage({
+          id: "alert.descriptionEmailInUse",
+        }),
       };
     case "auth/wrong-password":
       return {
-        title: "Incorrect password",
-        description:
-          "The password you entered does not seem to match the password needed for this account.",
+        title: intl.formatMessage({
+          id: "alert.titleWrongPassword",
+        }),
+        description: intl.formatMessage({
+          id: "alert.descriptionWrongPassword",
+        }),
       };
     case "auth/user-not-found":
       return {
-        title: "Email address not found",
-        description:
-          "The email address you entered does not seem to match to an account in this application.",
+        title: intl.formatMessage({
+          id: "alert.titleUserNotFound",
+        }),
+        description: intl.formatMessage({
+          id: "alert.descriptionUserNotFound",
+        }),
       };
     default:
       return {
-        title: "Internal error",
-        description: "An internal error has appeared, please try again later.",
+        title: intl.formatMessage({
+          id: "alert.titleInternalError",
+        }),
+        description: intl.formatMessage({
+          id: "alert.descriptionInternalError",
+        }),
       };
   }
 };
